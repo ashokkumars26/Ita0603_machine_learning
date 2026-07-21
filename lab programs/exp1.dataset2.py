c1569f59a0259947bb2d4fb9d1ad00ebe828cd9f
Ashok Kumar <ashokkumars2601@gmail.com>
2026-07-21 11:31:01 +0530

    Add files via upload

---
+++ lab programs/exp1.dataset2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = pd.read_csv("loan_approval.csv")
+
+attributes = data.iloc[:, :-1].values
+target = data["LoanApproved"].values
+
+hypothesis = ['Ø'] * len(attributes[0])
+
+for i in range(len(attributes)):
+    if target[i] == "Yes":
+        if hypothesis[0] == 'Ø':
+            hypothesis = list(attributes[i])
+        else:
+            for j in range(len(hypothesis)):
+                if hypothesis[j] != attributes[i][j]:
+                    hypothesis[j] = '?'
+
+print("Final Hypothesis:")
+print(hypothesis)
