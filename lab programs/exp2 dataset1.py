c01d2439ca44580c3a768211f1b9457a52ba4f14
Ashok Kumar <ashokkumars2601@gmail.com>
2026-07-23 14:35:15 +0530

    Add files via upload

---
+++ lab programs/exp2 dataset1.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("play_tennis.csv")
+
+concepts = np.array(data.iloc[:, :-1])
+target = np.array(data.iloc[:, -1])
+
+specific = concepts[0].copy()
+general = [["?" for _ in range(len(specific))] for _ in range(len(specific))]
+
+print("Initial Specific Hypothesis:", specific)
+print("Initial General Hypothesis:", general)
+
+for i, h in enumerate(concepts):
+    if target[i] == "Yes":
+        for x in range(len(specific)):
+            if h[x] != specific[x]:
+                specific[x] = "?"
+                general[x][x] = "?"
+    else:
+        for x in range(len(specific)):
+            if h[x] != specific[x]:
+                general[x][x] = specific[x]
+            else:
+                general[x][x] = "?"
+
+print("\nFinal Specific Hypothesis:")
+print(specific)
+
+print("\nFinal General Hypothesis:")
+general = [g for g in general if g != ["?"] * len(specific)]
+for g in general:
+    print(g)
